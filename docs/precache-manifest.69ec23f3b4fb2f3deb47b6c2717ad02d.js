fabc708311d5704c5c45d4ea82bd8534c3fa108c
Celiagm82 <celiagm82@gmail.com>
2020-04-25 23:52:50 +0200

    add docs

---
+++ docs/precache-manifest.69ec23f3b4fb2f3deb47b6c2717ad02d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb0e9b730c5422e1bcf108e45728aa7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7607521434a6a1ccc7d7",
+    "url": "./static/css/main.3ab83b63.chunk.css"
+  },
+  {
+    "revision": "1060e5fb90f757f6cd3a",
+    "url": "./static/js/2.248fa896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.248fa896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7607521434a6a1ccc7d7",
+    "url": "./static/js/main.221e5593.chunk.js"
+  },
+  {
+    "revision": "a126dcc470197a062998",
+    "url": "./static/js/runtime-main.bb193d6c.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  },
+  {
+    "revision": "c24fcbb6840dba90abb4f8c6689c9840",
+    "url": "./static/media/starfall.c24fcbb6.gif"
+  }
+]);
